ffe48ae4f5c10ef0c9e3ac22173ffac88c79b78c
Puneeth-Tester <emailtopuneeth@gmail.com>
2024-11-03 01:53:16 +0530

    Grid Parallel Execution

--- src/main/java/com/example/demo/libraries/WebDriverLibrary.java
+++ src/main/java/com/example/demo/libraries/WebDriverLibrary.java
@@ -9,9 +9,11 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
+@Profile("!remote")
 public class WebDriverLibrary {
 
     @Bean
